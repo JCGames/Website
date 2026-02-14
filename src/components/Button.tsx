6dfcb678c772e1955265a432b6ff113e2074b907
Christian Marcellino <cammar12177@gmail.com>
2026-02-14 14:31:28 -0600

    Reset project styles

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+type ButtonProps = {
+    label: string
+}
+
+export default function Button(props: ButtonProps) {
+    return (
+        <button>
+            {props.label}
+        </button>
+    );
+}
